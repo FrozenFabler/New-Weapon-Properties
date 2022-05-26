8c4452de60a179ca811dd04a1f34253d41e8c154
FrozenFabler <106294477+FrozenFabler@users.noreply.github.com>
2022-05-26 00:23:06 -0400

    Create weaponproperties.js

---
+++ weaponproperties.js
@@ -0,0 +1,17 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['bandolier'] = 'Bandolier';
+  CONFIG.DND5E.weaponProperties['brutal'] = 'Brutal';
+	CONFIG.DND5E.weaponProperties['critical'] = 'Critical';
+  CONFIG.DND5E.weaponProperties['concealed'] = 'Concealed';
+	CONFIG.DND5E.weaponProperties['defensive'] = 'Defensive';
+	CONFIG.DND5E.weaponProperties['disarming'] = 'Disarming';
+	CONFIG.DND5E.weaponProperties['deadly'] = 'Deadly';
+  CONFIG.DND5E.weaponProperties['firm grip'] = 'Firm Grip';
+  CONFIG.DND5E.weaponProperties['flanged'] = 'Flanged';
+	CONFIG.DND5E.weaponProperties['Natatorial'] = 'Natatorial';
+  CONFIG.DND5E.weaponProperties['runic'] = 'runic';
+	CONFIG.DND5E.weaponProperties['Siege'] = 'Siege';
+	CONFIG.DND5E.weaponProperties['Tripping'] = 'Tripping';
+	CONFIG.DND5E.weaponProperties['Wounding'] = 'Wounding';
+	
+});
